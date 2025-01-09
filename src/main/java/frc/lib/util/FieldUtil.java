b03901662f8ee7702b46d1fe6608dab6051e7439
awfulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2025-01-09 23:07:24 +0000

    blue and red poses for fieldutils (#2)

--- src/main/java/frc/lib/util/FieldUtil.java
+++ src/main/java/frc/lib/util/FieldUtil.java
@@ -10,39 +10,49 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
 public class FieldUtil {
 	/* all locations are relative */
-	public static final Pose2d ProcessorOpening = new Pose2d(Units.inchesToMeters(235.73),
+	public static final Pose2d BlueProcessorOpening = new Pose2d(Units.inchesToMeters(235.73),
 		Units.inchesToMeters(-0.15), Rotation2d.fromDegrees(90));
+	public static final Pose2d RedProcessorOpening = new Pose2d(Units.inchesToMeters(455.15),
+		Units.inchesToMeters(317.15), Rotation2d.fromDegrees(270));
 
-	public static final Pose2d LeftCoralStation = new Pose2d(Units.inchesToMeters(33.51),
+	public static final Pose2d BlueLeftCoralStation = new Pose2d(Units.inchesToMeters(33.51),
 		Units.inchesToMeters(291.20), Rotation2d.fromDegrees(306));
-
-	public static final Pose2d RightCoralStation = new Pose2d(Units.inchesToMeters(33.51),
+	public static final Pose2d BlueRightCoralStation = new Pose2d(Units.inchesToMeters(33.51),
 		Units.inchesToMeters(25.80), Rotation2d.fromDegrees(54));
 
-	public static final Pose2d FarReef = new Pose2d(Units.inchesToMeters(209.49),
-		Units.inchesToMeters(158.50), Rotation2d.fromDegrees(0));
+	public static final Pose2d RedLeftCoralStation = new Pose2d(Units.inchesToMeters(657.37),
+		Units.inchesToMeters(291.20), Rotation2d.fromDegrees(126));
+	public static final Pose2d RedRightCoralStation = new Pose2d(Units.inchesToMeters(657.37),
+		Units.inchesToMeters(25.80), Rotation2d.fromDegrees(234));
 
-	public static final Pose2d NearReef = new Pose2d(Units.inchesToMeters(144.00),
-		Units.inchesToMeters(158.50), Rotation2d.fromDegrees(180));
-
-	/* center of our barge and edge of the net we can score in */
-	public static final Pose2d BargeCenter = new Pose2d(Units.inchesToMeters(690.875 / 2.0 - 46/2.0), Units.inchesToMeters(241.64), Rotation2d.fromDegrees(180));
+	/* center of our barge and edge of the net we can score in from our side */
+	public static final Pose2d BlueBargeCenter = new Pose2d(Units.inchesToMeters(690.875 / 2.0 - 46/2.0), Units.inchesToMeters(241.64), Rotation2d.fromDegrees(180));
+	public static final Pose2d RedBargeCenter = new Pose2d(Units.inchesToMeters(690.875 / 2.0 + 46/2.0), Units.inchesToMeters(75.39), Rotation2d.fromDegrees(0));
 	public static final double BargeWidth = Units.inchesToMeters(146.50);
 
-	/* counter clockwise
+	/* reef locations starting from farthest reef and going counter clockwise
 	 * a "reef" is a set of two "prongs" where coral can be scored.  there are 6 sets of these
 	 * in a hexagonal pattern
 	*/
-	public static final Pose2d ReefPoses[] = {
-		FarReef,
+	public static final Pose2d BlueReefPoses[] = {
+		new Pose2d(Units.inchesToMeters(209.49), Units.inchesToMeters(158.50), Rotation2d.fromDegrees(0)),
 		new Pose2d(Units.inchesToMeters(193.10), Units.inchesToMeters(186.83), Rotation2d.fromDegrees(60)),
 		new Pose2d(Units.inchesToMeters(160.39), Units.inchesToMeters(186.83), Rotation2d.fromDegrees(120)),
-		NearReef,
+		new Pose2d(Units.inchesToMeters(144.00), Units.inchesToMeters(158.50), Rotation2d.fromDegrees(180)),
 		new Pose2d(Units.inchesToMeters(160.39), Units.inchesToMeters(130.17), Rotation2d.fromDegrees(240)),
 		new Pose2d(Units.inchesToMeters(193.10), Units.inchesToMeters(130.17), Rotation2d.fromDegrees(300)),
 	};
 
-	boolean isRedAlliance() {
+	public static final Pose2d RedReefPoses[] = {
+		new Pose2d(Units.inchesToMeters(481.39), Units.inchesToMeters(158.50), Rotation2d.fromDegrees(180)),
+		new Pose2d(Units.inchesToMeters(497.77), Units.inchesToMeters(130.17), Rotation2d.fromDegrees(240)),
+		new Pose2d(Units.inchesToMeters(530.49), Units.inchesToMeters(130.17), Rotation2d.fromDegrees(300)),
+		new Pose2d(Units.inchesToMeters(546.87), Units.inchesToMeters(158.50), Rotation2d.fromDegrees(0)),
+		new Pose2d(Units.inchesToMeters(530.49), Units.inchesToMeters(186.83), Rotation2d.fromDegrees(60)),
+		new Pose2d(Units.inchesToMeters(497.77), Units.inchesToMeters(186.83), Rotation2d.fromDegrees(120)),
+	};
+
+	public static boolean isRedAlliance() {
 		Optional<Alliance> alliance = DriverStation.getAlliance();
 
 		if (alliance.isEmpty())
@@ -53,5 +63,40 @@ public class FieldUtil {
 		}
 
 		return false;
+	}
+
+	public static Pose2d[] getReef() {
+		if (isRedAlliance())
+			return RedReefPoses;
+
+		return BlueReefPoses;
+	}
+
+	public static Pose2d getProcessor() {
+		if (isRedAlliance())
+			return RedProcessorOpening;
+
+		return BlueProcessorOpening;
+	}
+
+	public static Pose2d getRightCoralStation() {
+		if (isRedAlliance())
+			return RedRightCoralStation;
+
+		return BlueRightCoralStation;
+	}
+
+	public static Pose2d getLeftCoralStation() {
+		if (isRedAlliance())
+			return RedLeftCoralStation;
+
+		return BlueLeftCoralStation;
+	}
+
+	public static Pose2d getBargeCenter() {
+		if (isRedAlliance())
+			return RedBargeCenter;
+
+		return BlueBargeCenter;
 	}
 }
