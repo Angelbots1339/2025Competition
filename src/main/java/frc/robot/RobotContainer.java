a76f03e7efad6321aeb9fbd3e26600c22c03a0d5
awfulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2025-01-11 16:35:09 +0000

    Swerve (#3)
    * sweve?
    
    * sim
    
    * pose + field2d
    
    * remove unnecessary stuff
    
    * auto because bored
    
    * blue origin
    
    apparntly the shouldflippath of pathplanner also rotates for this year's
    game
    
    * track closest coral scoring location
    
    * pathfinding to the coral
    
    * pathfind to coral station
    
    * reef selection
    
    * fix red human coral station angles

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,17 +4,82 @@
 
 package frc.robot;
 
+import java.util.function.Supplier;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.subsystems.Swerve;
 
 public class RobotContainer {
-  public RobotContainer() {
-    configureBindings();
-  }
+	private final XboxController m_joystick = new XboxController(0);
+	private final Supplier<Double> leftX = () -> -m_joystick.getLeftX();
+	private final Supplier<Double> leftY = () -> -m_joystick.getLeftY();
+	private final Supplier<Double> rightX = () -> -m_joystick.getRightX();
 
-  private void configureBindings() {}
+	private Swerve swerve = new Swerve();
 
-  public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
-  }
+	private Trigger moveToClosestReef = new Trigger(() -> m_joystick.getAButton());
+	private Trigger moveToSelectedReef = new Trigger(() -> m_joystick.getYButton());
+	private Trigger leftCoralStation = new Trigger(() -> m_joystick.getBButton());
+	private Trigger rightCoralStation = new Trigger(() -> m_joystick.getXButton());
+
+	private Trigger selectReef = new Trigger(() -> m_joystick.getPOV() != -1);
+
+
+	private final SendableChooser<Command> autoChooser;
+
+	public RobotContainer() {
+		configureBindings();
+
+		autoChooser = AutoBuilder.buildAutoChooser("Mobility");
+		SmartDashboard.putData(autoChooser);
+	}
+
+	private void configureBindings() {
+		moveToClosestReef.whileTrue(Commands.deferredProxy(() -> swerve.driveToClosestReef()));
+		leftCoralStation.whileTrue(Commands.deferredProxy(() -> swerve.driveToLeftCoralStation()));
+		rightCoralStation.whileTrue(Commands.deferredProxy(() -> swerve.driveToRightCoralStation()));
+		moveToSelectedReef.whileTrue(Commands.deferredProxy(() -> swerve.driveToSelectedReef()));
+		selectReef.onTrue(
+				Commands.runOnce(() -> {
+					int reef = 0;
+					switch (m_joystick.getPOV()) {
+						case 0:
+							reef = 0;
+							break;
+						case 45:
+							reef = 1;
+							break;
+						case 90:
+							reef = 2;
+							break;
+						case 180:
+							reef = 3;
+							break;
+						case 270:
+							reef = 4;
+							break;
+						case 315:
+							reef = 5;
+							break;
+						default:
+							return;
+					}
+					swerve.selectReef(reef);
+				}, swerve)
+		);
+
+		swerve.setDefaultCommand(Commands.run(() -> {
+			swerve.drive(leftY, leftX, rightX, true);
+		}, swerve));
+	}
+
+	public Command getAutonomousCommand() {
+		return autoChooser.getSelected();
+	}
 }
---
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -0,0 +1,251 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.util.function.Supplier;
+
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.swerve.SwerveDrivetrain;
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveModule.SteerRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+import com.pathplanner.lib.path.PathConstraints;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.lib.util.FieldUtil;
+import frc.lib.util.PoseEstimation;
+import frc.robot.generated.TunerConstants;
+
+public class Swerve extends SubsystemBase {
+	public SwerveDrivetrain<TalonFX, TalonFX, CANcoder> swerve = TunerConstants.Swerve;
+	private final Field2d m_field = new Field2d();
+
+	private double maxspeed = 3;
+	private double maxturn = Math.PI * 2;
+
+	private double coralScoreOffsetY = Units.inchesToMeters(0);
+	private double coralScoreOffsetX = Units.inchesToMeters(24);
+
+	private Pose2d selectedReef = null;
+
+	private final SwerveRequest.ApplyRobotSpeeds autoRequest = new SwerveRequest.ApplyRobotSpeeds()
+		.withDriveRequestType(DriveRequestType.Velocity);
+	private final SwerveRequest.RobotCentric m_robotRequest = new SwerveRequest.RobotCentric()
+			.withDeadband(maxspeed * 0.1).withRotationalDeadband(Math.PI / 2 * 0.1)
+			.withDriveRequestType(DriveRequestType.OpenLoopVoltage)
+			.withSteerRequestType(SteerRequestType.MotionMagicExpo);
+
+	/** Creates a new Swerve. */
+	public Swerve() {
+		configPathPlanner();
+		swerve.getPigeon2().setYaw(0);
+
+		putSwerveState();
+		SmartDashboard.putData("Field", m_field);
+	}
+
+	public void drive(Supplier<Double> x, Supplier<Double> y, Supplier<Double> turn, boolean fieldRelative) {
+		ChassisSpeeds speeds = new ChassisSpeeds(x.get() * maxspeed, y.get() * maxspeed, turn.get() * maxturn);
+		SwerveRequest req;
+
+		if (fieldRelative) {
+			speeds = ChassisSpeeds.fromFieldRelativeSpeeds(speeds, getRelativeYaw());
+		}
+
+		req = m_robotRequest
+			.withVelocityX(speeds.vxMetersPerSecond)
+			.withVelocityY(speeds.vyMetersPerSecond)
+			.withRotationalRate(speeds.omegaRadiansPerSecond);
+
+		swerve.setControl(req);
+	}
+
+	void resetPose(Pose2d pose) {
+		swerve.resetPose(pose);
+	}
+
+	void setChassisSpeeds(ChassisSpeeds speeds) {
+		swerve.setControl(autoRequest.withSpeeds(speeds));
+	}
+
+	ChassisSpeeds getRobotRelativeSpeeds() {
+		return swerve.getState().Speeds;
+	}
+
+	private void configPathPlanner() {
+		RobotConfig config = null;
+		try {
+			config = RobotConfig.fromGUISettings();
+		} catch (Exception e) {
+			// Handle exception as needed
+			e.printStackTrace();
+		}
+		AutoBuilder.configure(
+				PoseEstimation::getEstimatedPose, // Robot pose supplier
+				this::resetPose, // Method to reset odometry (will be called if your auto has a starting pose)
+				this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+				(speeds, feedforwards) -> setChassisSpeeds(speeds), // Method that will drive the robot given ROBOT
+																		// RELATIVE ChassisSpeeds. Also optionally
+																		// outputs individual module feedforwards
+				new PPHolonomicDriveController( // PPHolonomicController is the built in path following controller for
+												// holonomic drive trains
+						new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+						new PIDConstants(5.0, 0.0, 0.0) // Rotation PID constants
+				),
+				config, // The robot configuration
+				() -> {
+					// Boolean supplier that controls when the path will be mirrored for the red
+					// alliance
+					// This will flip the path being followed to the red side of the field.
+					// THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+					// var alliance = DriverStation.getAlliance();
+					// if (alliance.isPresent()) {
+					// 	return alliance.get() == DriverStation.Alliance.Red;
+					// }
+					// return FieldUtil.isRedAlliance() && !DriverStation.isTeleop();
+					return FieldUtil.isRedAlliance();
+				},
+				this // Reference to this subsystem to set requirements
+		);
+	}
+
+	public Rotation2d getYaw() {
+		return swerve.getPigeon2().getRotation2d();
+	}
+
+	public Rotation2d getRelativeYaw() {
+		return FieldUtil.isRedAlliance() ? getYaw().rotateBy(Rotation2d.k180deg) : getYaw();
+	}
+
+	public void setYaw(Rotation2d angle) {
+		swerve.getPigeon2().setYaw(angle.getDegrees());
+	}
+
+	public Rotation2d getModuleAngle(int i) {
+		return swerve.getModule(i).getCurrentState().angle;
+	}
+
+	public double getModuleSpeed(int i) {
+		return swerve.getModule(i).getCurrentState().speedMetersPerSecond;
+	}
+
+	@Override
+	public void periodic() {
+		PoseEstimation.updateEstimatedPose(swerve.getState().Pose, this);
+
+		m_field.setRobotPose(PoseEstimation.getEstimatedPose());
+		m_field.getObject("closest reef").setPose(getClosestReef());
+		if (selectedReef != null)
+			m_field.getObject("selected reef").setPose(selectedReef);
+	}
+
+	public Pose2d getClosestReef() {
+		double dist = 10000000;
+		int close = 0;
+		Pose2d[] reefs = FieldUtil.getReef();
+		for (int i = 0; i < reefs.length; i++) {
+			Pose2d reef = reefs[i];
+			double distToReef = PoseEstimation.getEstimatedPose().getTranslation().getDistance(reef.getTranslation());
+			if (distToReef < dist) {
+				dist = distToReef;
+				close = i;
+			}
+		}
+
+		return reefs[close];
+	}
+
+	public Pose2d getReefScoreSpot(Pose2d reef) {
+		Translation2d tmp = new Translation2d(coralScoreOffsetX, coralScoreOffsetY);
+		tmp.rotateBy(reef.getRotation());
+
+		reef = reef.plus(new Transform2d(tmp.getX(), tmp.getY(), Rotation2d.k180deg));
+		return reef;
+	}
+
+	public void selectReef(int i) {
+		this.selectedReef = FieldUtil.getReef()[i];
+	}
+
+	public Pose2d getSelectCoral() {
+		return this.selectedReef;
+	}
+
+	public Command driveToPose(Pose2d target) {
+		PathConstraints constraints = new PathConstraints( 3.0, 4.0,
+				maxturn, Units.degreesToRadians(720));
+		return AutoBuilder.pathfindToPose(target, constraints, 0.0);
+	}
+
+	public Command driveToClosestReef() {
+		return driveToPose(getReefScoreSpot(getClosestReef()));
+	}
+
+	public Command driveToSelectedReef() {
+		if (selectedReef == null)
+			return Commands.none();
+
+		return driveToPose(getReefScoreSpot(selectedReef));
+	}
+
+	public Command driveToLeftCoralStation() {
+		return driveToPose(FieldUtil.getLeftCoralStation());
+	}
+
+	public Command driveToRightCoralStation() {
+		return driveToPose(FieldUtil.getRightCoralStation());
+	}
+
+
+	@Override
+	public void simulationPeriodic() {
+		/* Assume 20ms update rate, get battery voltage from WPILib */
+		swerve.updateSimState(0.020, RobotController.getBatteryVoltage());
+	}
+
+	public void putSwerveState() {
+		SmartDashboard.putData("Swerve Drive", new Sendable() {
+			@Override
+			public void initSendable(SendableBuilder builder) {
+				builder.setSmartDashboardType("SwerveDrive");
+				builder.addDoubleProperty("Front Left Angle", () -> getModuleAngle(0).getDegrees(), null);
+				builder.addDoubleProperty("Front Left Velocity", () -> getModuleSpeed(0), null);
+
+				builder.addDoubleProperty("Front Left Angle", () -> getModuleAngle(0).getDegrees(), null);
+				builder.addDoubleProperty("Front Left Velocity", () -> getModuleSpeed(0), null);
+
+				builder.addDoubleProperty("Front Right Angle", () -> getModuleAngle(1).getDegrees(), null);
+				builder.addDoubleProperty("Front Right Velocity", () -> getModuleSpeed(1), null);
+
+				builder.addDoubleProperty("Back Left Angle", () -> getModuleAngle(2).getDegrees(), null);
+				builder.addDoubleProperty("Back Left Velocity", () -> getModuleSpeed(2), null);
+
+				builder.addDoubleProperty("Back Right Angle", () -> getModuleAngle(3).getDegrees(), null);
+				builder.addDoubleProperty("Back Right Velocity", () -> getModuleSpeed(3), null);
+
+				builder.addDoubleProperty("Robot Angle", () -> getYaw().getDegrees(), null);
+			}
+		});
+	}
+}
