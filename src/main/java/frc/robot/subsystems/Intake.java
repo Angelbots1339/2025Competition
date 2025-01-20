41ea4b054fc5cab8e12fe9bdba177f66864297c8
awfulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2025-01-20 03:08:23 -0700

    add mech2d for intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -8,8 +8,12 @@ import java.util.function.Supplier;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.util.logging.LoggedSubsystem;
@@ -27,12 +31,23 @@ public class Intake extends SubsystemBase {
 	private LoggedFalcon loggedAngle;
 	private LoggedFalcon loggedWheel;
 
+	private Mechanism2d intake = new Mechanism2d(Units.inchesToMeters(20), Units.inchesToMeters(30));
+	private MechanismLigament2d slapdown;
+
 	public Intake() {
 		angleMotor.getConfigurator().apply(IntakeConstants.angleConfigs);
 
 		changeAngle(() -> IntakeConstants.insideAngle);
 
 		initLogging();
+
+		setMech();
+
+	}
+
+	public void setMech() {
+		slapdown = intake.getRoot("Intake", Units.inchesToMeters(10.5), 0).append(new MechanismLigament2d("Base", Units.inchesToMeters(10), 90)).append(new MechanismLigament2d("Slapdown", Units.inchesToMeters(20), 0));
+		SmartDashboard.putData("Intake Mech", intake);
 	}
 
 	public void changeAngle(Supplier<Angle> angle) {
@@ -68,6 +83,7 @@ public class Intake extends SubsystemBase {
 
 	@Override
 	public void periodic() {
+		slapdown.setAngle(-90 + getAngle().in(Degrees));
 	}
 
 	public void initLogging() {
