13e9bb4cfcb45a42375694845e414561a67014cf
awfulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2025-01-22 15:36:33 -0700

    hack to improve odemetry further because why not

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -80,7 +80,7 @@ public class TunerConstants {
 
     private static final double kDriveGearRatio = 6.75;
     private static final double kSteerGearRatio = 12.8;
-    public static final Distance kWheelRadius = Inches.of(2);
+    public static final Distance kWheelRadius = Inches.of(2 * (1.0 - 0.0338361));
 
     private static final boolean kInvertLeftSide = false;
     private static final boolean kInvertRightSide = true;
