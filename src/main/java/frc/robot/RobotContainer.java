0a359d6555eb44b3a2df5bcaf095683254620285
awfulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2025-03-04 20:56:44 -0700

    reenable swerve

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -220,7 +220,7 @@ public class RobotContainer {
 	}
 
 	public void setDefaultCommands() {
-		// swerve.setDefaultCommand(swerve.drive(leftY, leftX, rightX, () -> true, () -> !elevator.isAtHome()));
+		swerve.setDefaultCommand(swerve.drive(leftY, leftX, rightX, () -> true, () -> !elevator.isAtHome()));
 		endeffector.setDefaultCommand(
 				Commands.run(() -> endeffector.home(), endeffector).onlyIf(() -> elevator.isAtHome())
 				.andThen(Commands.run(() -> endeffector.hold(), endeffector)));
